cf5b402b579cc44afa5ba61f823027e675e67c5c
Thanh Tam <137189690+0796738860@users.noreply.github.com>
2024-06-23 15:28:12 +0700

    web

--- webbanhang/app/views.py
+++ webbanhang/app/views.py
@@ -7,6 +7,23 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 
 # Create your views here.
+def detail(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order,created = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+        user_not_login = 'hidden'
+        user_login = 'show'
+    else:
+        items = []
+        order = {'get_cart_items':0,'get_cart_total':0}
+        cartItems = order['get_cart_items']
+        user_not_login = 'show'
+        user_login = 'hidden'
+    categories = Category.objects.filter(is_sub = False)
+    context = {'categories':categories,'items':items,'order':order,'cartItems':cartItems,'user_not_login':user_not_login,'user_login':user_login}
+    return render(request,'app/detail.html',context)
 def category(request):
     categories = Category.objects.filter(is_sub = True)
     active_category = request.GET.get('category','')
